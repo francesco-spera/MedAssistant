dae705afae1a6a575878af2a4d19a4fa2c4ac609
raffycaccia <45554975+raffycaccia@users.noreply.github.com>
2020-01-14 17:46:30 +0100

    Fix EmailSender

--- MEDASSISTANT/src/control/gestioneEmail/EmailSender.java
+++ MEDASSISTANT/src/control/gestioneEmail/EmailSender.java
@@ -1,6 +1,17 @@
 package control.gestioneEmail;
 import java.io.IOException;
+import java.util.Date;
+import java.util.Properties;
 
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,6 +22,7 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/EmailSender")
 public class EmailSender extends HttpServlet {
+	
 
 	private static final long serialVersionUID = 1L;
 	private String host;
@@ -28,6 +40,42 @@ public class EmailSender extends HttpServlet {
         user = context.getInitParameter("user");
         pass = context.getInitParameter("pass");
     }
+    
+    public static void sendEmail(String host, String port,
+            final String userName, final String password, String toAddress,
+            String subject, String message) throws AddressException,
+            MessagingException {
+ 
+        // sets SMTP server properties
+        Properties properties = new Properties();
+        properties.put("mail.smtp.host", host);
+        properties.put("mail.smtp.port", port);
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+ 
+        // creates a new session with an authenticator
+        Authenticator auth = new Authenticator() {
+            public PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(userName, password);
+            }
+        };
+ 
+        Session session = Session.getInstance(properties, auth);
+ 
+        // creates a new e-mail message
+        Message msg = new MimeMessage(session);
+ 
+        msg.setFrom(new InternetAddress(userName));
+        InternetAddress[] toAddresses = { new InternetAddress(toAddress) };
+        msg.setRecipients(Message.RecipientType.TO, toAddresses);
+        msg.setSubject(subject);
+        msg.setSentDate(new Date());
+        msg.setText(message);
+ 
+        // sends the e-mail
+        Transport.send(msg);
+ 
+    }
  
     protected void doPost(HttpServletRequest request,
             HttpServletResponse response) throws ServletException, IOException {
@@ -41,7 +89,7 @@ public class EmailSender extends HttpServlet {
 
 		 
 		        try {
-		            EmailUtility.sendEmail(host, port, user, pass, email, subject,
+		            sendEmail(host, port, user, pass, email, subject,
 		                    content);
 		            System.out.println("email inviata");
 		        } catch (Exception ex) {
